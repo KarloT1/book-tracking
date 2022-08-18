ccf5f54d153698552731e1b4214600999130380d
karlotomasic9@gmail.com <karlotomasic9@gmail.com>
2022-08-18 17:04:45 +0200

    Add database, images, and test request.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+  componentDidMount() {
+    fetch("http://localhost:3000/books", {
+      "Content-Type": "application/json"
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
